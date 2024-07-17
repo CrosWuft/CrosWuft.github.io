2c0e3c2c19c210a1182446c3432e03dc482c64f2
cold-su <coldsu@outlook.com>
2024-07-17 14:50:13 +0800

    Create test.vue

---
+++ vue/test.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    // data() 返回的属性将会成为响应式的状态
+    // 并且暴露在 `this` 上
+    data() {
+        return {
+            count: 0
+        }
+    },
+
+    // methods 是一些用来更改状态与触发更新的函数
+    // 它们可以在模板中作为事件处理器绑定
+    methods: {
+        increment() {
+            this.count++
+        }
+    },
+
+    // 生命周期钩子会在组件生命周期的各个不同阶段被调用
+    // 例如这个函数就会在组件挂载完成后被调用
+    mounted() {
+        console.log(`The initial count is ${this.count}.`)
+    }
+}
+</script>
+
+<template>
+    <button @click="increment">Count is: {{ count }}</button>
+</template>
