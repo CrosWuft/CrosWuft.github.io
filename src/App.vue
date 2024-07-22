f5a88d61b9c2b52f67a49fd1eabf772d3b0abc00
cold-su <coldsu@outlook.com>
2024-07-23 04:14:45 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,10 @@ import FirstArticle from './views/FirstArticle.vue';
 </script>
 
 <template>
-    <!--     <header>
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-        <div class="wrapper">
-            <HelloWorld msg="You did it!" />
-
-            <nav>
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
-            </nav>
+    <div class="main">
+        <div class="main-content">
+            <SiteHeader />
+            <FirstArticle />
         </div>
-    </header>
-    <RouterView />
- -->
-    <SiteHeader />
-    <FirstArticle />
+    </div>
 </template>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,43 +1,39 @@
 <template>
-    <div class="main">
-        <div class="main-content">
-            <div class="site-header">
-                <a href="https://croswuft.github.io">
-                    <img class="avatar"
-                        src="https://cdn.jsdelivr.net/gh/CrosWuft/CrosWuft.github.io@main/assets/avatar.png?v=1706801474406"
-                        alt="">
-                </a>
-                <h1 class="site-title">
-                    CrosWuft
-                </h1>
-                <p class="site-description">
-                    欢迎您来到本站,很高兴遇见你!本站由CrosWuft创建,目前是DT服务器的官网,以后或许会推出新功能，敬请期待！
-                </p>
-                <div class="menu-container">
+    <div class="site-header">
+        <a href="https://croswuft.github.io">
+            <img class="avatar"
+                src="https://cdn.jsdelivr.net/gh/CrosWuft/CrosWuft.github.io@main/assets/avatar.png?v=1706801474406"
+                alt="">
+        </a>
+        <h1 class="site-title">
+            CrosWuft
+        </h1>
+        <p class="site-description">
+            欢迎您来到本站,很高兴遇见你!本站由CrosWuft创建,目前是DT服务器的官网,以后或许会推出新功能，敬请期待！
+        </p>
+        <div class="menu-container">
 
 
-                    <a href="post/welcome/index.html" class="menu">
-                        首页
-                    </a>
+            <a href="post/welcome/index.html" class="menu">
+                首页
+            </a>
 
-                    <a href="/post/about" class="menu">
-                        关于
-                    </a>
+            <a href="/post/about" class="menu">
+                关于
+            </a>
 
-                    <a href="post/others/index.html" class="menu">
-                        其它
-                    </a>
+            <a href="post/others/index.html" class="menu">
+                其它
+            </a>
 
-                    <a href="post/dt/index.html" class="menu">
-                        地铁村庄
-                    </a>
-                </div>
-                <div class="social-container">
-                    <a href="https://github.com/CrosWuft" target="_blank">
-                        <i class="ri-github-line"></i>
-                    </a>
-                </div>
-            </div>
+            <a href="post/dt/index.html" class="menu">
+                地铁村庄
+            </a>
+        </div>
+        <div class="social-container">
+            <a href="https://github.com/CrosWuft" target="_blank">
+                <i class="ri-github-line"></i>
+            </a>
         </div>
     </div>
 </template>
